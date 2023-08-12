e78af67d002848c9d68fd0ccb8947687ab61b4bc
Sand-raM <umurazawigasabo@gmail.com>
2023-08-11 19:48:33 -0700

    ./7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char d;
+
+	d = 'z';
+	while
+		(d >= 'a') {
+			putchar(d);
+			d--;
+		}
+	putchar('\n');
+	return (0);
+}
