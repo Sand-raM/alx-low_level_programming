affb6e5146186dfd5755c2723684c1d48cc227f0
Sand-raM <umurazawigasabo@gmail.com>
2023-08-11 20:04:30 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char b;
+
+	int c;
+
+	b = 'a';
+	c = 0;
+	while
+		(c < 10) {
+			putchar(c + '0');
+			c++;
+		}
+	while
+		(b <= 'f') {
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
