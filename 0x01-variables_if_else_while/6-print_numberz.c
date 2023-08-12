4c26d9eb1645fbe2005eb66288cc0e7516f3e7ec
Sand-raM <umurazawigasabo@gmail.com>
2023-08-11 19:38:21 -0700

    ./6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Definately 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 10) {
+			putchar(a + '0');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
