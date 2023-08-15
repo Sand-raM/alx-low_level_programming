9c50fc9ffd6691eff67a66de1ed24ef107c4eee7
Sand-raM <umurazawigasabo@gmail.com>
2023-08-14 19:02:32 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * AbSV - Computes the absolute value of an integer
+ * @c: Number to be cuomputed
+ * Return: Absolute value of num or zero
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_v;
+
+		abs_v = c * -1;
+		return (abs_v);
+	}
+	return (c);
+}
