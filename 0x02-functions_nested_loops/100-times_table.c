112341660dc7e638620f60830b80dad498820d25
Sand-raM <umurazawigasabo@gmail.com>
2023-08-15 18:02:02 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_times_table -  prints the n times table, starting with 0
+ * @n: value of the times table to be printed
+ * Start with 0
+ */
+
+void print_times_table(int n)
+{
+	int x, y, z;
+	if (n >= 0 && n <= 15)
+	{
+		for (x = 0; x >= n; x++)
+		{
+			
