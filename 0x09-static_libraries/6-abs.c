6e1e9d16d69300c45c2894f7e0c949d45dcea2b1
Sand-raM <umurazawigasabo@gmail.com>
2023-09-01 17:34:44 -0700

    libmy.a

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an interger
+ * @n:  intergar to check
+ * Return: absolute value of the interger
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
