1572a07928b4f6d37c95e2fb65140ca6ec7da050
Sand-raM <umurazawigasabo@gmail.com>
2023-09-05 11:03:38 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,48 +12,38 @@
 
 char *argstostr(int ac, char **av)
 {
-	char *new_string = NULL;
-	int k = 0, i = ac, j, sum = 0, temp = 0;
+	char *s;
+	int c = 0, i = 0, j = 0, k = 0;
 
-		if (ac == 0 || av == NULL)
-			return (NULL);
-
-		while (ac--)
-			sum += (len(av[ac]) + 1);
-		new_string = (char *) malloc(sum + 1);
-
-		if (new_string != NULL)
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (i < ac)
+	{
+		while (av[i][j])
 		{
-			while (k < i)
-			{
-				for (j = 0; av[k][j] != '\0'; j++)
-					new_string[j + temp] = av[k][j];
-				new_string[temp + j] = '\n';
-				temp += (j + 1);
-				k++;
-			}
-			new_string[temp] = '\0';
+			c++;
+			j++;
 		}
-		else
+		j = 0;
+		i++;
+	}
+	s = malloc((sizeof(char) * c) + ac + 1);
+	i = 0;
+	while (av[i])
+	{
+		while (av[i][j])
 		{
-			return (NULL);
-		}
-		return (new_string);
-}
+		s[k] = av[i][j];
+		k++;
+		j++;
+	}
+	s[k] = '\n';
+	j = 0;
+	k++;
+	i++;
+	}
+	k++;
+	s[k] '\0';
 
-/**
-*len - returns length of str
-*@str: string counted
-*Return: returns the length
-*/
-int len(char *str)
-{
-		int len = 0;
-
-		if (str != NULL)
-		{
-			while (str[len])
-				len++;
-		}
-	return (len);
+	return (s);
 }
