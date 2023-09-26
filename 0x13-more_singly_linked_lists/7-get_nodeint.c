2c9bb2206609298b16db9aa8bef214f76e2a4317
Sand-raM <umurazawigasabo@gmail.com>
2023-09-25 18:07:21 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (c = 0; c < index; c++)
 	{
 		head = head->next;
+		if (head == NULL)
+			return (NULL);
 	}
 	return (head);
 }
